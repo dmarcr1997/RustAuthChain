c2f8b501ba6b0c2722566c8124ac986bfa824f70
Damon Marc Rocha II <dmarcr1997@gmail.com>
2024-01-28 23:00:22 -0600

    Created rust project

---
+++ rusty_chain/target/debug/deps/rusty_chain-581659795e616fa2.d
@@ -0,0 +1,5 @@
+C:\Users\dmarc\Documents\Software\Rust\RustAuthChain\rusty_chain\target\debug\deps\librusty_chain-581659795e616fa2.rmeta: src\main.rs
+
+C:\Users\dmarc\Documents\Software\Rust\RustAuthChain\rusty_chain\target\debug\deps\rusty_chain-581659795e616fa2.d: src\main.rs
+
+src\main.rs:
---
+++ rusty_chain/target/debug/deps/rusty_chain-e95d4ceecb4cf5cb.d
@@ -0,0 +1,5 @@
+C:\Users\dmarc\Documents\Software\Rust\RustAuthChain\rusty_chain\target\debug\deps\librusty_chain-e95d4ceecb4cf5cb.rmeta: src\main.rs
+
+C:\Users\dmarc\Documents\Software\Rust\RustAuthChain\rusty_chain\target\debug\deps\rusty_chain-e95d4ceecb4cf5cb.d: src\main.rs
+
+src\main.rs:
